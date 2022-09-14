8aa3ca40e869b2830a9d91646b53723afa996561
jhdcruz <jhdcrux@outlook.com>
2022-09-14 13:54:31 +0800

    initial table view

--- src/view/TableView.java
+++ src/view/TableView.java
@@ -10,21 +10,23 @@ import javax.swing.JTable;
 public class TableView extends JFrame {
     private JPanel PANEL_MAIN;
     private JTable TABLE_STUDENTS;
-    private JButton ENTRY_ADD;
+    private JButton entryAdd;
     private JScrollPane TABLE_SCROLLPANE;
     private JPanel PANEL_TABLE;
     private JPanel CONTROL_PANEL;
     private JFormattedTextField ENTRY_SEARCH;
-    private JButton ENTRY_EDIT;
-    private JButton ENTRY_DELETE;
+    private JButton entryEdit;
+    private JButton entryDelete;
+    private JButton adminSignOut;
+    private JButton adminButton;
 
     public TableView() {
         setTitle("Student Management");
         setContentPane(PANEL_MAIN);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
 
         setSize(1000, 600);
-        setVisible(true);
         pack();
     }
 }
